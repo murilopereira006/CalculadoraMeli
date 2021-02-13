1fbeea85495342c3167ae70655486cafa912ca7a
murilopereira006 <muriloedu006@gmail.com>
2021-02-13 19:03:06 -0300

    resetando para refatoracao

--- CalculadoraMeli/src/App.js
+++ CalculadoraMeli/src/App.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import './App.css';
-import Header from './componentes/Header';
-import Inputs from './componentes/Inputs';
 
 function App() {
   return (
     <div>
-      <Header />
-      <Inputs />
+      Refatorando
     </div>
   );
 }
