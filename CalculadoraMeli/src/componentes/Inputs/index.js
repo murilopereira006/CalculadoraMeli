5cd3b48763d2f28b1252ee21e6fd363714a387da
murilopereira006 <muriloedu006@gmail.com>
2021-02-14 01:14:59 -0300

    forms completo

--- CalculadoraMeli/src/componentes/Inputs/index.js
+++ CalculadoraMeli/src/componentes/Inputs/index.js
@@ -6,10 +6,64 @@ function Inputs() {
 
   return (
 		<form>
-			<input
-				type='text'
-				placeholder='bota aqui!'
-			/>
+			<label htmlFor="nomeProduto">
+				Titulo do anuncio:
+				<input
+					name="nomeProduto"
+					type="text"
+					placeholder="Ex.: Tenis Nike Vapormax"
+				/>
+			</label>
+			<label htmlFor="peso">
+				Peso em quilos:
+				<input
+					name="peso"
+					type="text"
+					placeholder="Kg"
+				/>
+			</label>
+			<label>
+				Categoria:
+				<select>
+					<option>
+						opcao 1
+					</option>
+					<option>
+						opcao 2
+					</option>
+					<option>
+						opcao 3
+					</option>
+					<option>
+						opcao 4
+					</option>
+					<option>
+						opcao 5
+					</option>
+				</select>
+			</label>
+			<label>
+				Reputacao:
+				<select>
+					<option>
+						Laranja
+					</option>
+					<option>
+						Amarelo
+					</option>
+					<option>
+						Verde
+					</option>
+				</select>
+			</label>
+			<label htmlFor="markUp">
+				Markup:
+				<input
+					name="markUp"
+					type="number"
+					placeholder="1.6"
+				/>
+			</label>
 		</form>
   );
 }
