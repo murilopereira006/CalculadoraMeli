f8044b2d1d2df8b39bf13129eb01f2044613634c
murilopereira006 <muriloedu006@gmail.com>
2021-02-14 01:35:14 -0300

    fetch Api

--- CalculadoraMeli/src/services/fetchApi.js
+++ CalculadoraMeli/src/services/fetchApi.js
@@ -1,13 +1,13 @@
 export async function getCategories() {
-    const categories = await fetch('https://api.mercadolibre.com/sites/MLB/categories')
-      .then((response) => response.json());
+    const response = await fetch('https://api.mercadolibre.com/sites/MLB/categories');
+		const categories = response.json();
     return categories;
   }
 
 export async function getProductsFromCategoryAndQuery(categoryId, query) {
-	const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`)
-		.then((data) => data.json());
-	return response;
+	const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`);
+	const product = response.json();
+	return product;
 }
 
 export async function getProductsFromQuery(query) {
